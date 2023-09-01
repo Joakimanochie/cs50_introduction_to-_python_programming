4bc11c861fff9f70475f817ef87b18953fffa959
Joakimanochie <anochietobe@gmail.com>
2023-09-01 21:41:09 +0100

    update directories

---
+++ Libraries/sayings.py
@@ -0,0 +1,12 @@
+def main():
+    goodbye("world")
+    hello("Malan")
+
+def hello(input):
+    print(f"hello, {input}")
+
+def goodbye(word):
+    print(f"goodbye, {word}")
+
+if __name__ == "__main__":
+    main()
