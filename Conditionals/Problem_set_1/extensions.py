7856d2bb3dc1c7f97f7b8a79ea5fc8b45c2522eb
Joakimanochie <anochietobe@gmail.com>
2023-09-05 03:16:31 +0100

    extensions update

--- Conditionals/Problem_set_1/extensions.py
+++ Conditionals/Problem_set_1/extensions.py
@@ -1,18 +1,24 @@
-file = input("File name: ")
+def main():
+    exe = input("File name: ")
+    print_exe(exe)
 
-if file.endswith(".jpeg") == True:
-    print("image/jpeg")
-elif file.endswith(".jpg") == True:
-    print("image/jpeg")
-elif file.endswith(".gif") == True:
-    print("image/gif")
-elif file.endswith(".png") == True:
-    print("image/png")
-elif file.endswith(".pdf") == True:
-    print("application/pdf")
-elif file.endswith(".txt") == True:
-    print("text/plain")
-elif file.endswith(".zip") == True:
-    print("application/zip")
-else:
-    print("application/octet-stream")
+def print_exe(file):
+    if file.endswith(".jpeg") == True:
+        print("image/jpeg")
+    elif file.endswith(".jpg") == True:
+        print("image/jpeg")
+    elif file.endswith(".gif") == True:
+        print("image/gif")
+    elif file.endswith(".png") == True:
+        print("image/png")
+    elif file.endswith(".pdf") == True:
+        print("application/pdf")
+    elif file.endswith(".txt") == True:
+        print("text/plain")
+    elif file.endswith(".zip") == True:
+        print("application/zip")
+    else:
+        print("application/octet-stream")
+
+if __name__ == "__main__":
+    main()
