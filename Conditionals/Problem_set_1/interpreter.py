2852fd7579c01de8780e8e345dc3dd07430abf25
Joakimanochie <anochietobe@gmail.com>
2023-10-02 14:57:46 +0100

    update

--- Conditionals/Problem_set_1/interpreter.py
+++ Conditionals/Problem_set_1/interpreter.py
@@ -1,4 +1,24 @@
-# Function to calculate the result of the arithmetic expression
+def main():
+    user_input = input("Expression: ")
+    parts = user_input.split()
+    if len(parts) != 3:
+        print("Invalid input format. Please enter an expression in the format 'x operator y'")
+    else:
+        try:
+            x = int(parts[0])
+            operator = parts[1]
+            y = int(parts[2])
+            
+            result = calculate_expression(x, operator, y)
+            if isinstance(result, str):
+                print(result)
+            else:
+                print(f"Result: {result:.1f}")
+        except ValueError:
+            print("Invalid input. Please enter valid integers for x and z.")
+
+
+
 def calculate_expression(x, operator, y):
     if operator == '+':
         return x + y
@@ -13,24 +33,6 @@ def calculate_expression(x, operator, y):
     else:
         return "Error: Invalid operator"
 
-# Prompt the user for input
-user_input = input("Enter an arithmetic expression (e.g., x + y): ")
 
-# Split the input into x, operator, and y
-parts = user_input.split()
-if len(parts) != 3:
-    print("Invalid input format. Please enter an expression in the format 'x operator y'")
-else:
-    try:
-        x = int(parts[0])
-        operator = parts[1]
-        y = int(parts[2])
-        
-        result = calculate_expression(x, operator, y)
-        if isinstance(result, str):
-            print(result)
-        else:
-            print(f"Result: {result:.1f}")
-    except ValueError:
-        print("Invalid input. Please enter valid integers for x and z.")
-
+if __name__ == "__main__":
+    main()
